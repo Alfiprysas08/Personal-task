e1abce5ecc79891252f8a4ba1d7205236fe08b1a
alfiprysas08 <alfiprysas08@gmail.com>
2026-07-20 13:16:04 +0700

    Debug dashboard finance sync

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import DashboardHeader from "@/components/dashboard/DashboardHeader";
 import StatsCard from "@/components/dashboard/StatsCard";
 import UpcomingTasks from "@/components/dashboard/UpcomingTasks";
